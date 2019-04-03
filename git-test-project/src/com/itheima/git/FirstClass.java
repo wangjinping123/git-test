9a1befaa5f942a4981e71a1893cdfa0cf3719974
zhangsan <zhangsan@itcast.cn>
2019-04-03 16:11:52 +0800

    修改git

--- git-test-project/src/com/itheima/git/FirstClass.java
+++ git-test-project/src/com/itheima/git/FirstClass.java
@@ -8,11 +8,13 @@ public class FirstClass {
         System.out.println("hello git");
         System.out.println("hello git");
         System.out.println("hello git");
+//        hello git
     }
 
     public void test(){
         int i = 0;
         i += 10;
+        System.out.println(i);
 
     }
 }
